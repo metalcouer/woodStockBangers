9cecbaf5c0fbd559373f039e0a2c8e0ba449bded
Diane Sanchez <diane@Dianes-MacBook-Air-2.local>
2019-01-18 11:45:02 -0700

    added 2 get routes and 500 error for catch all)

--- app.js
+++ app.js
@@ -15,5 +15,23 @@ app.get('/data', (req, res, next) => {
     })
 })
 
+app.get('/:tags', (req, res, next) => {
+    const day = req.params.tags
+   
+    if(!data.day.includes(day)){
+        res.status(404).send('No One Played This Day')
+     }
+     else{
+         const matching = data.headliners.filter(headliners => headliners.day.includes(day))
+         res.status(200).send(matching)
+        }
+})
+
+app.use((req,res, next) => {
+    res.status(500).send('Something Went Terribly Wrong, Sorry.')
+})
+
+app.post('/newArtist', (req, res) => res.send('New artist has been added'))
+
 
 app.listen(port, () => console.log(`Ayyyy hmu on ${port}`))
